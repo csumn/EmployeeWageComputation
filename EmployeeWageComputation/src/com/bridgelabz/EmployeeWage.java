36894e8473754b1613355f3ba52e31c9be4c6d29
csumn <csumn13@gmail.com>
2022-03-02 17:41:12 +0530

    made some changes on Switch case Program

--- EmployeeWageComputation/src/com/bridgelabz/EmployeeWage.java
+++ EmployeeWageComputation/src/com/bridgelabz/EmployeeWage.java
@@ -2,7 +2,7 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 
-	static final int WAGE_PER_HOUR=20;
+	static final int WAGE_PER_HOUR_IS=20;
 	static final int IS_FULL_TIME=1;
 	static final int IS_PART_TIME=2;
 
@@ -13,31 +13,18 @@ public class EmployeeWage {
 		switch (value) {
 		case IS_FULL_TIME: {
 			int empHrs = 8;
-			int empWage = WAGE_PER_HOUR * empHrs; 
+			int empWage = WAGE_PER_HOUR_IS * empHrs; 
 			System.out.println("Employee is Present (Full Time) and Wage is :" +empWage);
 			break;
 			}
 		case IS_PART_TIME: {
 			int empHrs = 4;
-			int empWage = WAGE_PER_HOUR * empHrs; 
+			int empWage = WAGE_PER_HOUR_IS * empHrs; 
 			System.out.println("Employee is Present (Part Time) and Wage is :" +empWage);
 			break;
 			}
 		default:
 			System.out.println("Employee is Absent ....");	
 		}
-		/*if(value == IS_FULL_TIME) {
-			int empHrs = 8;
-			int empWage = WAGE_PER_HOUR * empHrs; 
-			System.out.println("Employee is Present (Full Time) and Wage is :" +empWage);
-		}
-		else if (value == IS_PART_TIME){
-			int empHrs = 4;
-			int empWage =WAGE_PER_HOUR * empHrs;
-			System.out.println("Employee is Present (Part Time) and Wage is :" +empWage);
-		}
-		else 
-		System.out.println("Employee is Absent ....");	
-		}*/
 	}
 }
