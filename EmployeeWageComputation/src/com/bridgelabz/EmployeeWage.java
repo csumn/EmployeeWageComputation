00995636454fd1925d7dc93612a9706b50e31b61
csumn <csumn13@gmail.com>
2022-03-02 16:06:36 +0530

    Daily Wage calculation Program Added

--- EmployeeWageComputation/src/com/bridgelabz/EmployeeWage.java
+++ EmployeeWageComputation/src/com/bridgelabz/EmployeeWage.java
@@ -2,13 +2,17 @@ package com.bridgelabz;
 
 public class EmployeeWage {
 
+	static final int WAGE_PER_HOUR=20;
+	
 	public static void main(String[] args) {
 		int isPresent = 1;
 		int value = (int)Math.floor(Math.random()*10) % 2;
 		if(value == isPresent) {
-		System.out.println("Employee is Present");
+			int empHrs = 8;
+			int empWage = WAGE_PER_HOUR*empHrs; 
+			System.out.println("Employee is Present and Wage is :" +empWage);
 		}
 		else 
-		System.out.println("Employee is Absent");	
+		System.out.println("Employee is Absent ....");	
 		}
 }
